b6689797907be642dbc9f33c3f08d75d51b280ae
SubLim3 <sublim3.js@hotmail.com>
2024-08-13 22:17:11 -0500

    Update move_files.sh

--- move_files.sh
+++ move_files.sh
@@ -6,21 +6,8 @@ mv -f ~/MagicMirror/config/config.js ~/MagicMirror/config/original_config.js
 # Move the SubLim3 custom config.js to the config folder.
 mv -f ~/SubLim3-MagicMirror/config.js ~/MagicMirror/config/config.js
 
-echo -----------------------------------
-echo "Custom config.js is set"
-echo -----------------------------------
-
-timeout /t 3
-
 # Rename config.js so the custom file can be added.
 mv -f ~/MagicMirror/modules/default/compliments/compliments.js ~/MagicMirror/modules/default/compliments/original_compliments.js
 
 # Move compliments.js to the compliments folder.
 mv -f ~/SubLim3-MagicMirror/compliments.js ~/MagicMirror/modules/default/compliments/compliments.js
-
-echo -----------------------------------
-echo "Custom compliments.js is set"
-echo -----------------------------------
-
-timeout /t 3
-
