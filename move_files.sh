0d1cdd6b9430bf9046c115ce9c9da20a47756301
SubLim3 <sublim3.js@hotmail.com>
2024-08-14 20:34:17 -0500

    Update move_files.sh

--- move_files.sh
+++ move_files.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
 # Rename config.js so the custom file can be added.
-mv -f ~/MagicMirror/config/config.js ~/MagicMirror/config/original_config.js
+if [ -f ~/MagicMirror/config/config.js ]; then
+    mv -f ~/MagicMirror/config/config.js ~/MagicMirror/config/original_config.js
+    echo "Default config.js has been archived."
+else
+    echo "File config.js not found in ~/MagicMirror/config/"
+fi
 
 # Move the SubLim3 custom config.js to the config folder.
-mv -f ~/SubLim3-MagicMirror/config.js ~/MagicMirror/config/config.js
+if [ -f ~/SubLim3-MagicMirror/config.js ]; then
+    mv -f ~/SubLim3-MagicMirror/config.js ~/MagicMirror/config/config.js
+    echo "SubLim3 config.js has been moved to the MagicMirror config folder."
+else
+    echo "File SubLim3 config.js not found in ~/SubLim3-MagicMirror/"
+fi
 
-# Rename config.js so the custom file can be added.
-mv -f ~/MagicMirror/modules/default/compliments/compliments.js ~/MagicMirror/modules/default/compliments/original_compliments.js
+# Rename compliments.js so the custom file can be added.
+if [ -f ~/MagicMirror/modules/default/compliments/compliments.js ]; then
+    mv -f ~/MagicMirror/modules/default/compliments/compliments.js ~/MagicMirror/modules/default/compliments/original_compliments.js
+    echo "Default compliments.js has been archived."
+else
+    echo "File compliments.js not found in ~/MagicMirror/modules/default/compliments/"
+fi
 
 # Move compliments.js to the compliments folder.
-mv -f ~/SubLim3-MagicMirror/compliments.js ~/MagicMirror/modules/default/compliments/compliments.js
+if [ -f ~/SubLim3-MagicMirror/compliments.js ]; then
+    mv -f ~/SubLim3-MagicMirror/compliments.js ~/MagicMirror/modules/default/compliments/compliments.js
+    echo "SubLim3 compliments.js has been moved to the MagicMirror compliments module folder."
+else
+    echo "File compliments.js not found in ~/SubLim3-MagicMirror/"
+fi
