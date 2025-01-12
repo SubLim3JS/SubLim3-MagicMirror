60021df7800fd860b0b1bacc8fda89b020692e86
SubLim3 <sublim3.js@hotmail.com>
2025-01-12 17:13:33 -0600

    Update Add-Compliments.sh
    Code update via ChatGPT.

--- Add-Compliments.sh
+++ Add-Compliments.sh
@@ -5,8 +5,13 @@ echo "/ ___| _   _| |__ | |   (_)_ __ ___ |___ /  |  \/  (_)_ __ _ __ ___  _ __ 
 echo "\___ \| | | | '_ \| |   | | '_ ` _ \  |_ \  | |\/| | | '__| '__/ _ \| '__|"
 echo " ___) | |_| | |_) | |___| | | | | | |___) | | |  | | | |  | | | (_) | |   "
 echo "|____/ \__,_|_.__/|_____|_|_| |_| |_|____/  |_|  |_|_|_|  |_|  \___/|_|   "
+sleep 5
 
-# Rename compliments.js so the custom file can be added.
+echo "**************************************************************"
+echo "*** Rename compliments.js so the custom file can be added. ***"
+echo "**************************************************************"
+sleep 5
+
 if [ -f ~/MagicMirror/modules/default/compliments/compliments.js ]; then
     mv -f ~/MagicMirror/modules/default/compliments/compliments.js ~/MagicMirror/modules/default/compliments/original_compliments.js
     echo "Default compliments.js has been archived."
@@ -14,7 +19,11 @@ else
     echo "File compliments.js not found in ~/MagicMirror/modules/default/compliments/"
 fi
 
-# Move custom compliments.js to the compliments folder.
+echo "*************************************************************"
+echo "*** Move custom compliments.js to the compliments folder. ***"
+echo "*************************************************************"
+sleep 5
+
 if [ -f ~/SubLim3-MagicMirror/compliments.js ]; then
     mv -f ~/SubLim3-MagicMirror/compliments.js ~/MagicMirror/modules/default/compliments/compliments.js
     echo "SubLim3 compliments.js has been moved to the MagicMirror compliments module folder."
