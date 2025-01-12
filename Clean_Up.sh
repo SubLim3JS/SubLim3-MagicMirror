9d0293d9183b78b1ab49923d01639445248d4b53
SubLim3 <sublim3.js@hotmail.com>
2025-01-12 17:01:10 -0600

    Update Clean_Up.sh
    Code Clean up via ChatGPT.

--- Clean_Up.sh
+++ Clean_Up.sh
@@ -6,17 +6,26 @@ echo "\___ \| | | | '_ \| |   | | '_ ` _ \  |_ \  | |\/| | | '__| '__/ _ \| '__|
 echo " ___) | |_| | |_) | |___| | | | | | |___) | | |  | | | |  | | | (_) | |   "
 echo "|____/ \__,_|_.__/|_____|_|_| |_| |_|____/  |_|  |_|_|_|  |_|  \___/|_|   "
 
-# Remove original_config.js from ~/MagicMirror/config/.
+echo "*****************************************************************"
+echo "**** Removing original_config.js from ~/MagicMirror/config/. ****"
+echo "*****************************************************************"
+sleep 5
+
 if [ -f ~/MagicMirror/config/original_config.js ]; then
-    rm -rf ~/MagicMirror/config/original_config.js
-    echo "The original_config.js has been removed."
+    rm -f ~/MagicMirror/config/original_config.js
+    echo "***** The original_config.js has been removed. *****"
+    sleep 5
 else
     echo "File original_config.js not found in ~/MagicMirror/config/"
 fi
 
-# Remove original_compliments.js from ~/MagicMirror/modules/default/compliments/.
+echo "***************************************************************************************"
+echo "** Removing original_compliments.js from ~/MagicMirror/modules/default/compliments/. **"
+echo "***************************************************************************************"
+sleep 5
+
 if [ -f ~/MagicMirror/modules/default/compliments/original_compliments.js ]; then
-    rm -rf ~/MagicMirror/modules/default/compliments/original_compliments.js
+    rm -f ~/MagicMirror/modules/default/compliments/original_compliments.js
     echo "The original_compliments.js has been removed."
 else
     echo "File original_compliments.js not found in ~/MagicMirror/modules/default/compliments/"
