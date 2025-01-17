81e06e5ee06967e6b92e3b025aff02a1962816b8
SubLim3 <sublim3.js@hotmail.com>
2025-01-17 06:26:54 -0600

    Update Add-Compliments.sh

--- Add-Compliments.sh
+++ Add-Compliments.sh
@@ -13,38 +13,38 @@ printf "
 
 sleep 3
 
-echo "**************************************************************\n"
-echo "*** Rename compliments.js so the custom file can be added. ***\n"
-echo "**************************************************************\n\n"
+echo "**************************************************************"\n
+echo "*** Rename compliments.js so the custom file can be added. ***"\n
+echo "**************************************************************"\n\n
 sleep 5
 
 if [ -f ~/MagicMirror/modules/default/compliments/compliments.js ]; then
     cp -f ~/MagicMirror/modules/default/compliments/compliments.js ~/MagicMirror/modules/default/compliments/original_compliments.js
     printf ""
-    echo " - Default compliments.js has been archived. - \n\n\n"
+    echo " - Default compliments.js has been archived. - "\n\n\n
     printf ""
 else
     printf ""
-    echo " - File compliments.js not found in ~/MagicMirror/modules/default/compliments/ - \n\n\n"
+    echo " - File compliments.js not found in ~/MagicMirror/modules/default/compliments/ - "\n\n\n
     printf ""
 fi
 
-echo "*************************************************************\n"
-echo "*** Move custom compliments.js to the compliments folder. ***\n"
-echo "*************************************************************\n\n"
+echo "*************************************************************"\n
+echo "*** Move custom compliments.js to the compliments folder. ***"\n
+echo "*************************************************************"\n\n
 sleep 5
 
 if [ -f ~/SubLim3-MagicMirror/compliments.js ]; then
     cp -f ~/SubLim3-MagicMirror/compliments.js ~/MagicMirror/modules/default/compliments/compliments.js
     printf ""
-    echo " - SubLim3 compliments.js has been moved to the MagicMirror compliments module folder. - \n\n\n"
+    echo " - SubLim3 compliments.js has been moved to the MagicMirror compliments module folder. - "\n\n\n
     printf ""
 else
     printf ""
-    echo " - File compliments.js not found in ~/SubLim3-MagicMirror/ - \n\n\n"
+    echo " - File compliments.js not found in ~/SubLim3-MagicMirror/ - "\n\n\n
     printf ""
 fi
 
-echo "****************************************************\n"
-echo "***  - All operations completed successfully.  - ***\n"
-echo "****************************************************\n\n"
+echo "****************************************************"\n
+echo "***  - All operations completed successfully.  - ***"\n
+echo "****************************************************"\n\n
