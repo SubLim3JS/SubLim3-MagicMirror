592ecf3fdb1c0885388d4314e81a215ee6b917e3
SubLim3 <sublim3.js@hotmail.com>
2025-01-17 06:43:44 -0600

    Update Clean_Up_Originals.sh

--- Clean_Up_Originals.sh
+++ Clean_Up_Originals.sh
@@ -13,36 +13,41 @@ printf "
 
 sleep 3
 
-printf "*****************************************************************"
-printf "**** Removing original_config.js from ~/MagicMirror/config/. ****"
-printf "*****************************************************************"
+printf "*****************************************************************\n"
+printf "**** Removing original_config.js from ~/MagicMirror/config/. ****\n"
+printf "*****************************************************************\n\n"
 
 sleep 3
 
 if [ -f ~/MagicMirror/config/original_config.js ]; then
     cp -f ~/MagicMirror/config/original_config.js
-    printf "***** The original_config.js has been removed. *****"
+    printf ""
+    printf " - The original_config.js has been removed.  - \n\n\n"
+    printf ""
 else
-    printf "File original_config.js not found in ~/MagicMirror/config/"
+    printf ""
+    printf " - File original_config.js not found in ~/MagicMirror/config/ - \n\n\n"
+    printf ""
 fi
 
 sleep 3
 
-printf "***************************************************************************************"
-printf "** Removing original_compliments.js from ~/MagicMirror/modules/default/compliments/. **"
-printf "***************************************************************************************"
+printf "***************************************************************************************\n"
+printf "** Removing original_compliments.js from ~/MagicMirror/modules/default/compliments/. **\n"
+printf "***************************************************************************************\n"
 
 sleep 3
 
 
 if [ -f ~/MagicMirror/modules/default/compliments/original_compliments.js ]; then
     cp -f ~/MagicMirror/modules/default/compliments/original_compliments.js
-    printf "The original_compliments.js has been removed."
+    printf " - The original_compliments.js has been removed. - \n\n\n"
 else
-    printf "File original_compliments.js not found in ~/MagicMirror/modules/default/compliments/"
+    printf " - File original_compliments.js not found in ~/MagicMirror/modules/default/compliments/ - \n\n\n"
 fi
 
 printf "****************************************************"
 printf "***  - All operations completed successfully.  - ***"
 printf "****************************************************"
-sleep 5
+printf ""
+sleep 3
